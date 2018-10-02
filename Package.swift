d21b233329327df0bbcd9077fe732edb7a7485ee
Dario <dcoletto.sw@gmail.com>
2018-10-02 13:47:55 +0200

    Fixed missing dependency

--- Package.swift
+++ Package.swift
@@ -2,22 +2,22 @@
 import PackageDescription
 
 let package = Package(
-  name: "rswift",
-  dependencies: [
-    .package(url: "https://github.com/kylef/Commander.git", from: "0.8.0"),
-    .package(url: "https://github.com/tomlokhorst/XcodeEdit", from: "2.4.1")
-    .package(url: "https://github.com/drmohundro/SWXMLHash.git", from: "4.0.0")
-  ],
-  targets: [
-    .target(
-      name: "rswift",
-      dependencies: ["RswiftCore"]
-    ),
-    .target(
-      name: "RswiftCore",
-      dependencies: ["Commander", "XcodeEdit", "SWXMLHash"]
-    ),
-    .testTarget(name: "RswiftCoreTests", dependencies: ["RswiftCore"]),
-  ],
-  swiftLanguageVersions: [4]
+    name: "rswift",
+    dependencies: [
+        .package(url: "https://github.com/kylef/Commander.git", from: "0.8.0"),
+        .package(url: "https://github.com/tomlokhorst/XcodeEdit", from: "2.4.1"),
+        .package(url: "https://github.com/drmohundro/SWXMLHash.git", from: "4.0.0")
+    ],
+    targets: [
+        .target(
+            name: "rswift",
+            dependencies: ["RswiftCore"]
+        ),
+        .target(
+            name: "RswiftCore",
+            dependencies: ["Commander", "XcodeEdit", "SWXMLHash"]
+        ),
+        .testTarget(name: "RswiftCoreTests", dependencies: ["RswiftCore"]),
+        ],
+    swiftLanguageVersions: [4]
 )
